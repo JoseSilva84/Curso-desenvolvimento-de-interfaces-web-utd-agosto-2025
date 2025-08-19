348e01cc63c1c85d502de442e176702be6501d49
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-08-19 13:49:56 -0300

    dados3.3

--- Aula10-js-intro/ola-mundo.js
+++ Aula10-js-intro/ola-mundo.js
@@ -2,8 +2,19 @@ const digitacaoNome = prompt('Digite seu nome:').toLocaleLowerCase();
 const nome = 'júnior';
 
 if (digitacaoNome === nome) {
+    alert(`Olá, ${digitacaoNome}!`);
     console.log(`Olá, ${digitacaoNome}!`);
 }
 else {
-    console.log(`Você não é ${nome}!`);
-} 
+    alert(`Você não é ${nome}!`);
+}
+
+const digitacaoIdade = prompt('Digite sua idade:');
+const idade = 30;
+
+if (digitacaoIdade >= idade) {
+    alert('Você tem a idade ideal do sistema!')
+}
+else {
+    alert('Você não tem a idade ideal do sistema!')
+}
