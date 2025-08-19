1b29178b8c1b9f78eaafd13aa5b331e0e02b727f
José Evangelista da Silva Filho <juniornyanata@gmail.com>
2025-08-18 21:16:00 -0300

    dados3.2

--- Aula10-js-intro/ola-mundo.js
+++ Aula10-js-intro/ola-mundo.js
@@ -1,1 +1,9 @@
-console.log("Olá, mundo!");
+const digitacaoNome = prompt('Digite seu nome:').toLocaleLowerCase();
+const nome = 'júnior';
+
+if (digitacaoNome === nome) {
+    console.log(`Olá, ${digitacaoNome}!`);
+}
+else {
+    console.log(`Você não é ${nome}!`);
+} 
